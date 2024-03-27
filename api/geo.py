c17355023ce4f40a83ad36ac4e60faf938fbb8ec
Grigoryy Remizov <grigoriyr2412@gmail.com>
2024-03-27 10:24:01 +0300

    feat: city possible variants added
    add my_city.py
    add not_my_city.py
    add inline//cities.py

--- api/geo.py
+++ api/geo.py
@@ -5,12 +5,19 @@ from utils import query
 BASE_URL = "https://api.openweathermap.org/geo/1.0/direct"
 
 
-def geolocation(city_name: str,country_code: str) -> tuple[str, float, float]:
-    query_data = {"q" : (city_name, country_code),
-                  "appid" : API_KEY}
+def geolocation(city_name: str, country_code: str, all: bool = False
+                ) -> tuple[str, float, float] | list[tuple[str, float, float]]:
+    query_data = {"q" : (city_name, country_code), "appid" : API_KEY}
     url = BASE_URL + "?" + query(query_data)
+    
+    resp = requests.get(url).json()
     try:
-        resp = requests.get(url).json()[0]
-        return resp["name"], resp["lat"], resp["lon"]
+        resp[0]
+        if not all:
+            resp = resp[0]
+            return resp["name"], resp["lat"], resp["lon"]
+        else:
+            data = [(city["name"], city["lat"], city["lon"]) for city in resp]
+            return data
     except IndexError:
         return None
--- handlers/weather/states/city.py
+++ handlers/weather/states/city.py
@@ -1,10 +1,16 @@
 from loader import bot
 from telebot.types import Message
 from states import AskFor
+from api import geolocation
+from database import User
+from keyboards.inline import city_variants
 from utils.logging import log_from
 
 
 @bot.message_handler(no_cmd = True, state = AskFor.city, valid_city = "soft")
 @log_from
 def take_city(message: Message):
-    bot.send_message(message.chat.id, "This city added to database")
+    user: User = User.get(username = message.from_user.username)
+    variants = geolocation(message.text, user.country_id, all = True)
+    bot.send_message(message.chat.id, "Cannot find such city, maybe you meant one of this:",
+                     reply_markup = city_variants(variants))
