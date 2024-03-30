61133203b641020020b260eb6c9cfa27c22bd116
Griogryy Remizov <grigoriyr2412@gmail.com>
2024-03-30 10:25:41 +0300

    fix: incorrect limit message

--- handlers/weather/states/forecast_range.py
+++ handlers/weather/states/forecast_range.py
@@ -9,6 +9,6 @@ from utils.logging import log_call
 def take_frange(call: CallbackQuery):
     with bot.retrieve_data(call.from_user.id, call.message.chat.id) as data:
         data["range"] = call.data
-    bot.edit_message_text("Now write limit of values to show:",
+    bot.edit_message_text("Now write quantity of values to show:",
                           call.message.chat.id, call.message.id)
     bot.set_state(call.from_user.id, AskFor.limit, call.message.chat.id)
