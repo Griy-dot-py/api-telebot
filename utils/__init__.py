5211396e16bb191fade0b316be17a58e9df44863
Grigoryy Remizov <grigoriyr2412@gmail.com>
2024-04-20 11:41:41 +0300

    refactor: utils filenames

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from . import logging
 
-from .set_bot_commands import set_all_commands
-from .make_query import query
-from .filters import set_custom_filters
+from .func_set_all_commands import set_all_commands
+from .func_query import query
+from .func_set_custom_filters import set_custom_filters
 from .func_two_floats import two_floats
